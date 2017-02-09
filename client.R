94a0ef4d46c70e307940c4d965034aab0182773e
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-02-09 11:57:02 +0100

    simplified a bit

--- client.R
+++ client.R
@@ -1,31 +1,16 @@
 # rockets client
 PORT <- 36000
-DONE <- F
-message('Run rocket("ur payload") 2 communicate with the server,
-        rocket("EXIT") 2 shut down client and server socket.')
-# Establish a connection 2 the server-side
-clientInit <- function(port=PORT) {
-  CON <<- socketConnection(host='localhost', port=port, server=F, blocking=T, open='r+')
-  print(paste0('Connected on port ', port))
-}
-# Shut client socket
-clientKill <- function() {
-  DONE <<- T
-  close(CON)
-  rm(CON, envir=globalenv())
-}
-# Send text thru the socket
+message('Run rocket("EXIT", CON) 2 shut down client and server socket.')
+
+# Establishing a connection 2 the server-side
+CON <- socketConnection(host='localhost', port=PORT, server=F, blocking=T, open='r+')
+print(paste0('Connected on port ', PORT))
+print(summary.connection(CON))
+
+# Sending text thru the connection n printing response
 rocket <- function(text) {
-  stopifnot(typeof(text) == 'character')
-  if (!exists('CON')) clientInit()
+  stopifnot(typeof(text) == 'character', exists('CON'))
   writeLines(text, CON)
-  if (text == 'EXIT') clientKill() 
-}
-# Starting the client
-clientInit()
-# Printing payload
-while (!DONE) {  # isIncomplete(CON) evaluates 2 F before response been sent 
-  data <- readLines(CON, 1)
-  print(data)
-  if (data == 'EXIT') clientKill()
+  print(readLines(CON, 1))
+  if (text == 'EXIT') close(CON) 
 }
--- server.R
+++ server.R
@@ -1,30 +1,15 @@
 # rockets server
 PORT <- 36000
-DONE <- F
-message('Run serverInit() to establish a socket connection, serverKill() to shut it down.')
-# Provide a socket server
-serverInit <- function(port=PORT) {
-  CON <<- socketConnection(host='localhost', port=port, server=T, blocking=T, open='r+')
-  print(paste0('Listening on port ', port))
-}
-# Shut a server socket
-serverKill <- function() {
-  DONE <<- T
-  close(CON)
-  rm(CON, envir=globalenv())
-}
-# Send text thru the socket
-rocket <- function(text) {
-  stopifnot(typeof(text) == 'character')
-  if (!exists('CON')) clientInit()
-  writeLines(text, CON)
-  if (text == 'EXIT') clientKill() 
-}
-# Starting the server
-serverInit()
-# Printing payload
-while (!DONE) {  # isIncomplete(CON) evaluates 2 F before response been sent 
+
+# Creating a server socket
+CON <- socketConnection(host='localhost', port=PORT, server=T, blocking=T, open='r+')
+print(paste0('Listening on port ', PORT))
+print(summary.connection(CON))
+
+# Handling connections
+while (isOpen(CON)) {
   data <- readLines(CON, 1)
+  writeLines(toupper(data), CON)  # some pure func 2 generate response
   print(data)
-  if (data == 'EXIT') serverKill()
+  if (length(data) > 0 && data == 'EXIT') close(CON)
 }
