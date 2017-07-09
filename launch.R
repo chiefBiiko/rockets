3bb7705c15f05ce6550f0aa094104f14523c1af6
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-09 20:40:04 +0200

    filepaths

--- launch.R
+++ launch.R
@@ -4,8 +4,10 @@ launch <- function(host='127.0.0.1', port=10419L) {  # username, peer,
   # hashToPort username...
   # create a socket connection to peer
   # start server child
-  PID <- sys::exec_background(cmd=sprintf('Rscript.exe %s', 'server.R'),
-                              args=c(host, port))
+  RSCRIPT <- normalizePath(file.path(R.home(), 'bin', 'Rscript.exe'))
+  RSERVER <- 'server.R'  # file.path(.libPaths()[1L], 'rockets', 'server.R')
+  PID <- sys::exec_background(cmd=RSCRIPT,
+                              args=c(RSERVER, host, port))
   # exit with server child PID & outbound rocket
   return(PID)
 }
